d9777318fda22f5ea95bd3377aabf3128d0161b9
FelPrim <alexprihodkos2896@gmail.com>
2026-05-16 22:37:49 +0300

    added info.txt for some unknown reason

--- client/main.js
+++ client/main.js
@@ -1,2 +1,9 @@
 "use strict";
+
 console.log("Hello, world!");
+const WS_URL = 'wss://localhost:9002';
+const ws = new WebSocket(WS_URL);
+
+ws.onopen = () => console.log('WS connected');
+ws.onmessage = (e) => console.log('MSG:', e.data);
+ws.onclose = () => console.log('WS closed');
