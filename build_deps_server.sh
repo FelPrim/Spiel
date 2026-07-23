567d15c6df5172a8c274624ea0a9615ee9c63a5e
FelPrim <alexprihodkos2896@gmail.com>
2026-07-23 21:03:32 +0300

    something-something

--- build_deps_server.sh
+++ build_deps_server.sh
@@ -8,10 +8,14 @@ autoconf
 	CFLAGS="-O3 -march=native -fno-strict-aliasing"
 make -j$(nproc)
 make install
-export PKG_CONFIG_PATH="$PWD/../../include/lib/pkgconfig:$PKG_CONFI_PATH"
+export PKG_CONFIG_PATH="$PWD/../../include/lib/pkgconfig:$PKG_CONFIG_PATH"
 cd ../h2o
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="$PWD/../../../include" ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DCMAKE_C_FLAGS="-march=native" \
+	-DCMAKE_INSTALL_PREFIX="$PWD/../../../include" \
+	-DDISABLE_LIBUV=ON \
+	..
 make libh2o-evloop -j$(nproc)
 make install
