86daef44265b9004ab59757b696d0fa7cfaef580
FelPrim <alexprihodkos2896@gmail.com>
2026-08-05 03:04:59 +0300

    working on auth

--- server/build_local.sh
+++ server/build_local.sh
@@ -3,6 +3,7 @@ g++ -std=c++20 -march=native -g \
     -DLIBUS_USE_OPENSSL \
     -I ../ext/native/include/uWebSockets \
     -I ../ext/native/include/uSockets \
+    -I ../ext/native/include \
     -I ../deps/uWebSockets/uSockets/boringssl/include \
     main.cpp \
     ../ext/native/lib/uSockets.a \
